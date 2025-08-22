18780b863e36cde02cf9fc4b29cd834fad3e41a7
SAmarjeet05 <amar2005.reet@gmail.com>
2025-08-23 01:32:02 +0530

    updated

--- Backend/app/data/fetch_news.py
+++ Backend/app/data/fetch_news.py
@@ -2,8 +2,29 @@ import requests
 import logging
 from supabase import create_client, Client
 from app.config import SUPABASE_URL, SUPABASE_KEY, NEWS_API_KEY
+import joblib
+import os
+import sklearn
+
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # folder of fetch_news.py
+MODEL_PATH = os.path.join(BASE_DIR, "sentiment_classifier.joblib")
+VECTORIZER_PATH = os.path.join(BASE_DIR, "count_vectorizer.joblib")
+
+classifier = joblib.load(MODEL_PATH)
+cv = joblib.load(VECTORIZER_PATH)
+
+
+def predict_sentiment(text: str) -> str:
+    """Predict sentiment using trained model"""
+    # Preprocess input same way as training
+    text_vector = cv.transform([text]).toarray()
+    prediction = classifier.predict(text_vector)[0]
+    return "positive" if prediction == 1 else "negative"
 
 def fetch_and_store_news(company_name, company_id):
     logging.info(f"Fetching news for {company_name} (company_id={company_id})")
@@ -26,9 +47,11 @@ def fetch_and_store_news(company_name, company_id):
         news_url = article["url"]
         published_at = article["publishedAt"]
 
-        sentiment = ("negative" if any(x in title.lower() for x in ["loss", "down", "lawsuit", "fraud", "resign"])
-                     else "positive" if any(x in title.lower() for x in ["profit", "up", "record", "raises", "growth"])
-                     else "neutral")
+        sentiment = predict_sentiment(title)
+
+        # sentiment = ("negative" if any(x in title.lower() for x in ["loss", "down", "lawsuit", "fraud", "resign"])
+        #              else "positive" if any(x in title.lower() for x in ["profit", "up", "record", "raises", "growth"])
+        #              else "neutral")
 
         logging.info(f"Inserting news: '{title[:60]}...' for company_id={company_id} with sentiment={sentiment}")
         supabase.table("news").insert({
